fixed gradient updating and model training

src/model/trainer.py
@@ -19,7 +19,7 @@ class Trainer:
 
         print("--- DATA LOADED ----")
         self.dataset = self.path_file.readlines()[:10]
-        self.test = self.dataset[-(int(0.1*self.dataset)):]
+        self.test = self.dataset[-(int(0.1*len(self.dataset))):]
         self.dataset = self.dataset[:len(self.dataset) - len(self.test)]
         generator1 = torch.Generator().manual_seed(42)
         self.train_, self.val = random_split(self.dataset, [0.8, 0.2], generator=generator1)
@@ -51,7 +51,7 @@ class Trainer:
         return nn_costs_tensor, optimal_costs_tensor
     
 
-    def create_pairwise_cost_paths(self, path):
+    def create_pairwise_cost_paths(self, path, is_train=True):
         decoded_path = decode_path(path, self.search.game.decodeMap)
         self.buffer.add_pairs_from_path(decoded_path=decoded_path, target=self.search.game.target)
 
@@ -62,9 +62,8 @@ class Trainer:
         for state, target, goal_map, cost_to_go in sample:
             preds.append(self.model(state, target, goal_map))
             targs.append(cost_to_go)
-        
         nn_costs_tensor = torch.stack(preds).squeeze().to(self.device)
-        optimal_costs_tensor = torch.tensor(targs).to(dtype=torch.float32).to(self.device)
+        optimal_costs_tensor = torch.tensor(targs, requires_grad=is_train, device=self.device, dtype=torch.float32)
 
         return nn_costs_tensor, optimal_costs_tensor
 
@@ -72,7 +71,7 @@ class Trainer:
 
     def train(self):
         for epoch in tqdm(range(0, self.epochs), desc="Epochs"):
-            for index, line in enumerate(tqdm(self.train_, desc="states")):
+            for index, line in enumerate(tqdm(self.train_, desc="train states")):
                 path = line.strip().split(",")
                 state = self.states[index]
                 self.search.reinit(state)
@@ -87,21 +86,19 @@ class Trainer:
                     loss.backward()
                     self.optimizer.step()
             with torch.no_grad():
-                for index, line in enumerate(self.val):
+                for index, line in enumerate(tqdm(self.val, desc="val states")):
                     path = line.strip().split(",")
                     state = self.states[index]
                     self.search.reinit(state)
                     for _ in range(self.times_to_sample):
                         self.optimizer.zero_grad()
-                        nn_costs_tensor, optimal_costs_tensor = self.create_pairwise_cost_paths(path=path)
+                        nn_costs_tensor, optimal_costs_tensor = self.create_pairwise_cost_paths(path=path, is_train=False)
                         loss = self.criterion(nn_costs_tensor, optimal_costs_tensor)
-                        loss.backward()
-                    loss = self.criterion(nn_costs_tensor, optimal_costs_tensor)
                     correct = ((nn_costs_tensor == optimal_costs_tensor).float().sum()) / len(nn_costs_tensor)
                     self.writer.add_scalar('Loss/validation', loss.item(), epoch)
                     self.writer.add_scalar('Accuracy/train', correct.item(), epoch)
             
-            torch.save(self.model.state_dict(),f'runs/experiment_{self.config.algo}/checkpoint/last.pt')
+            torch.save(self.model.state_dict(),f'runs/experiment_{self.config.algo}/last.pt')
        
                         
 